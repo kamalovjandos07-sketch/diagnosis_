3a98482735bec0ae49d44f300cc2fd8d119af09b
kamalovjandos07-sketch <kamalovjandos07@gmail.com>
2026-01-20 10:59:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -257,8 +257,7 @@ def medical_diagnosis_system(symptoms, lab_data, vital_signs, temperature, bp_sy
 # ОСНОВНОЙ ИНТЕРФЕЙС
 def main():
     st.title("Медицинский справочник KazNMU")
-    st.title("Камалов Жандос ОМ24-015")
-    st.markdown("**Комплексная система диагностики и рекомендаций по лечению**")
+    st.markdown("**Комплексная система диагностики и рекомендаций по лечению [Камалов Жандос ОМ24-015]**")
     
     # ВВОД ДАННЫХ
     col1, col2 = st.columns(2)
